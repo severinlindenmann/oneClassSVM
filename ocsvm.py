d8ef059afe02711ac2fb4ab1578e129f54ae6218
Severin Lindenmann <github@severin.io>
2023-04-28 15:31:12 +0200

    update

--- ocsvm.py
+++ ocsvm.py
@@ -76,40 +76,43 @@ with tab1.expander('Code', expanded=False):
 
 ### Dataset
 ## Check if the dataset is selected and load the dataset
-if datasets == 'Spam Mail':
-    if LOCAL == 'FALSE':
-        tab2.info('The dataset is too large to be loaded and process live in the cloud, we will show you the preprocessed dataset and model instead, you still can choose a different kernel but the parameters will be ignored')
-    tab2.subheader('Spam Mail')
-    tab2.write('The dataset is loaded using the read_csv function from the pandas module. The dataset contains 5329 emails. The emails are divided into 2 classes: spam and ham.')
-    tab2.write('3900 no spam (ham) and 1896 is spam.')
-    tab2.write('Source: https://www.kaggle.com/datasets/ganiyuolalekan/spam-assassin-email-classification-dataset?resource=download')
-    
-    update = True
-    if update:
+dts_button = tab2.button('Load Dataset')
+
+if dts_button:
+    if datasets == 'Spam Mail':
         if LOCAL == 'FALSE':
-            spam_mail_cloud(tab2, kernel, nu, gamma, degree, score)
+            tab2.info('The dataset is too large to be loaded and process live in the cloud, we will show you the preprocessed dataset and model instead, you still can choose a different kernel but the parameters will be ignored')
+        tab2.subheader('Spam Mail')
+        tab2.write('The dataset is loaded using the read_csv function from the pandas module. The dataset contains 5329 emails. The emails are divided into 2 classes: spam and ham.')
+        tab2.write('3900 no spam (ham) and 1896 is spam.')
+        tab2.write('Source: https://www.kaggle.com/datasets/ganiyuolalekan/spam-assassin-email-classification-dataset?resource=download')
+        
+        update = True
+        if update:
+            if LOCAL == 'FALSE':
+                spam_mail_cloud(tab2, kernel, nu, gamma, degree, score)
 
-        if LOCAL == 'TRUE':
-            with st.spinner('It will take a while to load the dataset and train the model, please have patience'):
-                spam_mail_local(tab2, kernel, nu, gamma, degree, score, outlier_fraction, gamma_scale)
+            if LOCAL == 'TRUE':
+                with st.spinner('It will take a while to load the dataset and train the model, please have patience'):
+                    spam_mail_local(tab2, kernel, nu, gamma, degree, score, outlier_fraction, gamma_scale)
 
-## Check if the dataset is selected and load the dataset
-if datasets == 'Credit Card Fraud':
-    if LOCAL == 'FALSE':
-        tab2.info('The dataset is too large to be loaded and process live in the cloud, we will show you the preprocessed dataset and model instead, you still can choose a different kernel but the parameters will be ignored')
-    tab2.subheader('Credit Card Fraud')
-    tab2.write('The dataset is loaded using the read_csv function from the pandas module. This dataset presents transactions that occurred in two days, where we have 492 frauds out of 284,807 transactions. The dataset is highly unbalanced, the positive class (frauds) account for 0.172% of all transactions.')
-    tab2.write('It contains only numerical input variables which are the result of a PCA transformation. The only two features "Time" and "Amount" have not been transformed with PCA. The feature "Time" contains the seconds elapsed between each transaction and the first transaction in the dataset. The feature "Amount" is the transaction Amount, this feature can be used for example-dependant cost-senstive learning. The feature "Class" is the response variable and it takes value 1 in case of fraud and 0 otherwise.')
-    tab2.write('Source: https://www.kaggle.com/datasets/mlg-ulb/creditcardfraud?resource=download')
-    
-    update = True
-    if update:
+    ## Check if the dataset is selected and load the dataset
+    if datasets == 'Credit Card Fraud':
         if LOCAL == 'FALSE':
-            credit_card_cloud(tab2, kernel, nu, gamma, degree, score)
+            tab2.info('The dataset is too large to be loaded and process live in the cloud, we will show you the preprocessed dataset and model instead, you still can choose a different kernel but the parameters will be ignored')
+        tab2.subheader('Credit Card Fraud')
+        tab2.write('The dataset is loaded using the read_csv function from the pandas module. This dataset presents transactions that occurred in two days, where we have 492 frauds out of 284,807 transactions. The dataset is highly unbalanced, the positive class (frauds) account for 0.172% of all transactions.')
+        tab2.write('It contains only numerical input variables which are the result of a PCA transformation. The only two features "Time" and "Amount" have not been transformed with PCA. The feature "Time" contains the seconds elapsed between each transaction and the first transaction in the dataset. The feature "Amount" is the transaction Amount, this feature can be used for example-dependant cost-senstive learning. The feature "Class" is the response variable and it takes value 1 in case of fraud and 0 otherwise.')
+        tab2.write('Source: https://www.kaggle.com/datasets/mlg-ulb/creditcardfraud?resource=download')
+        
+        update = True
+        if update:
+            if LOCAL == 'FALSE':
+                credit_card_cloud(tab2, kernel, nu, gamma, degree, score)
 
-        if LOCAL == 'TRUE':
-            with st.spinner('It will take a while to load the dataset and train the model, please have patience'):
-                credit_card_local(tab2, kernel, nu, gamma, degree, score, outlier_fraction, gamma_scale)
+            if LOCAL == 'TRUE':
+                with st.spinner('It will take a while to load the dataset and train the model, please have patience'):
+                    credit_card_local(tab2, kernel, nu, gamma, degree, score, outlier_fraction, gamma_scale)
 
 
 if LOCAL == 'TRUE':
